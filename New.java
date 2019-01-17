7b469e45afc5bd35f9d15f21fd9ce1238518647d
Jay <kakkadjay@yahoo.in>
2019-01-17 10:05:49 +0530

    Add

---
+++ New.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class New{
+	public static void main(String[] arg){
+		Scanner scan = new Scanner(System.in);
+		double a,b,res;
+		char choice,ch;
+		do{
+			System.out.println("Enter two numbers");
+			a = scan.nextInt();
+			b = scan.nextInt();
+			System.out.println("1. Add");
+			System.out.println("2. Exit");
+			choice = scan.next().charAt(0);
+			switch(choice){
+				case '1': System.out.println(a+b);
+					  break;
+				case '2': System.exit(0);
+					  break;
+				default : System.out.println("Invalid choice");
+					  break;
+			}
+		}while(choice != 2);
+	}
+}
